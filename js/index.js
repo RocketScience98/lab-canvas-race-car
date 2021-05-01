89231a6f1771ebdf154693460a30c9382204eb06
Luis <luisalbertodavila@gmail.com>
2021-04-30 20:24:12 -0500

    falto la direccion de los obstaculos

--- js/index.js
+++ js/index.js
@@ -1,7 +1,175 @@
-window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
-    startGame();
-  };
+const car = new Image();
+car.src = './images/car.png';
+const pista = new Image();
+pista.src = './images/road.png';
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
 
-  function startGame() {}
+const myGameArea = {
+  frames: 0,
+  score: function () {
+      const points = Math.floor(this.frames / 5);
+      context.font = '40px serif';
+      context.fillStyle = 'white';
+      context.fillText(`Score: ${points}`, 75, 50);
+  },
+};
+class Component {
+  constructor(width, height, color, x, y) {
+      this.width = width;
+      this.height = height;
+      this.color = color;
+      this.x = x;
+      this.y = y;
+      this.dx = 0;
+      this.dy = 0;
+      this.speed = 10;
+    }
+    left() {
+      return this.x;
+    }
+    right() {
+      return this.x + this.width;
+    }
+    top() {
+      return this.y;
+    }
+    bottom() {
+      return this.y + this.height;
+    }
+    crashWith(obstacle) {    
+      return !(
+          this.bottom() < obstacle.top() ||
+          this.top() > obstacle.bottom() || 
+          this.right() < obstacle.left() || 
+          this.left() > obstacle.right());
+    }
+  update() {  // GENERA UNA FIGURA COLOR ROJO
+    context.fillStyle = this.color;
+    context.fillRect(this.x, this.y, this.width, this.height);
+  }
+  newPos() { // new Position del jugador o del obstáculo
+      this.x += this.speed;
+      this.y += this.speed;
+  }
+}
+const backgroundImage = {
+    img: pista,
+    x: 0,
+    y: 0,
+    speed: 1,
+    move: function() {
+        /*  this.x += this.speed
+         this.x %= text.width; */
+        this.y += this.speed
+        this.y %= canvas.height;
+    },
+    draw: function() {
+        context.drawImage(this.img, 0, this.y, canvas.width, canvas.height);
+        context.drawImage(this.img, 0, this.y - canvas.height, canvas.width, canvas.height);
+    }
+}
+const carro = new Component(50, 100,'',250,600);
+
+const borrar = () => {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+}
+const imagenes = () => {
+    //  context.drawImage(pista, 0, 0, canvas.width, canvas.height);
+    context.drawImage(car, carro.x, carro.y, carro.width, carro.height);
+}
+const keyUp = () => {
+    carro.dx = 0;
+}
+
+const moveRight = () => {
+    console.log("me movi a la derecha");
+    carro.dx = carro.speed;
+}
+const moveLeft = () => {
+    console.log("me movi a la izquierda");
+    carro.dx -= carro.speed;
+}
+const keyDown = (event) => {
+    switch (event.key) {
+        case "ArrowRight":
+            moveRight();
+            break;
+        case "ArrowLeft":
+            moveLeft();
+            break;
+        default:
+            return;
+    }
+}
+const detectWalls = () => {
+    if (carro.x < 60) {
+        carro.x = 60;
+    }
+    if ((carro.x + carro.width) > canvas.width - 60) {
+        carro.x = (canvas.width - carro.width) - 60;
+    }
+}
+const newPos = () => {
+  carro.x += carro.dx;
+}
+//Obstaculos
+const myObstacles=[]
+function updateObstacles() {
+  for (let i = 0; i < myObstacles.length; i++) {
+      myObstacles[i].y += 3;
+      myObstacles[i].update();
+  }
+  myGameArea.frames += 1;
+  if (myGameArea.frames % 120 === 0) {
+      let x = canvas.width;
+      let minWidth = 80;
+      let maxWidth = 320;
+      let width = Math.floor(Math.random() * (maxWidth - minWidth + 1) + minWidth);
+      //                               w    h   x   y   dx  speed
+      //  myObstacles.push(new Component(width, 10, 50, 50, 50, 0));
+      myObstacles.push(new Component(width, 10, 63, 0, 0, 0));
+      let minWidth2 = 80;
+      let maxWidth2 = 320;
+      let width2 = Math.floor(Math.random() * (maxWidth2 - minWidth2 + 1) + minWidth2);
+      myObstacles.push(new Component(-width2, 10, 440, 200, 200, 0));
+      console.log();
+  }
+}
+function reset(){
+  carro.x = 250
+  
+}
+
+function checkGameOver(id) {
+  const crashed = myObstacles.some(function (obstacle) {
+      return carro.crashWith(obstacle); // DEVUELVE UN VERDADERO O FALSO
+  });
+  if (crashed) {
+      cancelAnimationFrame(id)
+  }
+}
+
+document.addEventListener('keydown', keyDown);
+document.addEventListener('keyup', keyUp);
+
+window.onload = () => {
+    document.getElementById('start-button').onclick = () => {
+      reset()  
+      startGame();
+      
+    };
+    function startGame() {
+        borrar();
+        backgroundImage.move();
+        backgroundImage.draw();
+        detectWalls();
+        imagenes();
+        updateObstacles();
+        myGameArea.score()
+        newPos();
+        setTimeout(function(){ alert("Hello"); }, 3000)
+        let frameId= requestAnimationFrame(startGame);
+        checkGameOver(frameId);
+    }
 };
